83a0055da19b8a837b770aff7e662de4eb76ace6
Kadiga6 <9510@holbertonstudents.com>
2025-05-29 23:05:27 +0000

    4

--- TypeScript/task_1/js/main.ts
+++ TypeScript/task_1/js/main.ts
@@ -27,4 +27,46 @@ const director1: Directors = {
   fullTimeEmployee: true,
   numberOfReports: 17,
 };
+
 console.log(director1);
+
+
+interface printTeacherFunction {
+	(firstName: string, lastName: string): string;
+}
+
+const printTeacher: printTeacherFunction = (firstName, lastName) => {
+	return '${firstName.charAt(0)}. ${lastName}';
+};
+
+console.log(printTeacher("John", "Doe"));
+
+
+// Interface décrivant le constructeur
+interface StudentConstructor {
+  new (firstName: string, lastName: string): StudentClassInterface;
+}
+
+// Interface décrivant la classe
+interface StudentClassInterface {
+  workOnHomework(): string;
+  displayName(): string;
+}
+
+// Implémentation de la classe
+class StudentClass implements StudentClassInterface {
+  constructor(private firstName: string, private lastName: string) {}
+
+  workOnHomework(): string {
+    return "Currently working";
+  }
+
+  displayName(): string {
+    return this.firstName;
+  }
+}
+
+// Exemple d'utilisation
+const student = new StudentClass("Alice", "Dupont");
+console.log(student.displayName()); // Affiche "Alice"
+console.log(student.workOnHomework()); // Affiche "Currently working"
