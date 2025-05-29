f9e357f1dcbe7caa4405ab0eb6484b1e18d6769d
Kadiga6 <9510@holbertonstudents.com>
2025-05-29 21:47:26 +0000

    2

--- TypeScript/task_1/js/main.ts
+++ TypeScript/task_1/js/main.ts
@@ -14,3 +14,17 @@ const teacher3: Teacher = {
   location: 'London',
   contract: false,
 };
+
+
+interface Directors extends Teacher {
+	numberOfReports: number;
+}
+
+const director1: Directors = {
+  firstName: 'John',
+  lastName: 'Doe',
+  location: 'London',
+  fullTimeEmployee: true,
+  numberOfReports: 17,
+};
+console.log(director1);
